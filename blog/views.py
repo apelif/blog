e3150467e1e47be39a5c320995e8538c7bda1a61
dev <atonezzz@gmail.com>
2015-07-02 17:39:50 +0800

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -11,11 +11,13 @@ from django.core.paginator import Paginator
 from django.core.paginator import EmptyPage
 from django.core.paginator import PageNotAnInteger
 
+
 def index(request):
     """首页
     """
     articles = article_paginator(request, 5)
     return render_to_response('index.html', {"articles": articles})
+
 
 def detail(request, id):
     """文章页
@@ -23,12 +25,14 @@ def detail(request, id):
     article = Article.objects.get(id=str(id))
     return render_to_response('detail.html', {'article': article})
 
+
 def login(request):
     """登录页
     """
     if request.session.get('username'):
         return redirect('/admin/')
     return render_to_response('login.html', RequestContext(request))
+
 
 def login_verify(request):
     """登录验证
@@ -48,6 +52,7 @@ def login_verify(request):
     else:
         return redirect('/login/')
 
+
 @login_required
 def article_del(request, id):
     """删除文章
@@ -56,6 +61,7 @@ def article_del(request, id):
     article.delete()
     return redirect('/admin/')
 
+
 @login_required
 def article_new(request):
     """新建文章
@@ -63,11 +69,13 @@ def article_new(request):
     title = request.GET.get('title', '')
     content = request.GET.get('content', '')
     if not title or not content:
-        return render_to_response('new.html',
+        return render_to_response(
+                'new.html',
                 {'title': title, 'content': content})
     article = Article(title=title, content=content)
     article.save()
     return redirect('/admin/')
+
 
 @login_required
 def article_edit(request, id):
@@ -79,7 +87,8 @@ def article_edit(request, id):
     title_mod = request.GET.get('title', '')
     content_mod = request.GET.get('content', '')
     if not title_mod or not content_mod:
-        return render_to_response('edit.html',
+        return render_to_response(
+                'edit.html',
                 {'title': title, 'content': content})
     if title == title_mod and content == content_mod:
         return redirect('/admin/')
@@ -91,6 +100,7 @@ def article_edit(request, id):
         return redirect('/admin/')
     article.update(title=title_mod, content=content_mod)
     return redirect('/admin/')
+
 
 def article_paginator(request, page_num):
     """文章分页器
