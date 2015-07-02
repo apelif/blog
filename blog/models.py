88d2c9dbb06f8a001299a8f0a4f79f936d755231
dev <atonezzz@gmail.com>
2015-07-02 17:39:35 +0800

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     category = models.CharField(max_length=50, blank=True)
@@ -11,11 +12,12 @@ class Article(models.Model):
     def __unicode__(self):
         return self.title
 
+
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
     created_time = models.DateTimeField(auto_now_add=True)
-    
+
     def __unicode__(self):
         return self.username
